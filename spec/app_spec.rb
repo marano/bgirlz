4fe578749a308dacdde61dbf1296c55d32821014
marano <thiagomarano@gmail.com>
2012-07-31 00:54:08 -0700

    Added tests to publish via file.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -20,10 +20,21 @@ Capybara.app = Controller
 Capybara.current_driver = :selenium
 
 describe 'Black Girls Code Website Publisher' do
-  it 'should allow me to update my website' do
+  it 'should allow me to publish my website' do
     visit '/'
     fill_in 'name', :with => 'Joana'
     fill_in 'html', :with => 'oi!'
+    click_button 'Publish my website'
+    page.text.should == 'oi!'
+  end
+
+  it 'should allow me to publish my website using a file' do
+    visit '/'
+    fill_in 'name', :with => 'Joana'
+    page_file = Tempfile.new('mypage.html')
+    page_file.write 'oi!'
+    page_file.flush
+    attach_file('page', page_file.path)
     click_button 'Publish my website'
     page.text.should == 'oi!'
   end
