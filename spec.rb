61a2eebcb5076e32fd8dca0246a36c48712b127f
marano <thiagomarano@gmail.com>
2012-09-07 19:37:09 -0700

    Specs for page list and delete page.

--- spec.rb
+++ spec.rb
@@ -19,7 +19,7 @@ Capybara.app = Controller
 
 Capybara.current_driver = :selenium
 
-describe 'Black Girls Code Website Publisher' do
+describe 'Black Girls Code Website Publisher', :js => true do
   it 'should allow me to publish my website' do
     visit '/'
     fill_in 'name', :with => 'Joana'
@@ -37,5 +37,32 @@ describe 'Black Girls Code Website Publisher' do
     attach_file('page', page_file.path)
     click_button 'Publish my website'
     page.text.should == 'oi!'
+  end
+
+  it 'should display my page at list page' do
+    visit '/'
+    fill_in 'name', :with => 'Joana'
+    fill_in 'html', :with => 'oi!'
+    click_button 'Publish my website'
+    visit '/list'
+    page.should have_content('Joana')
+    page.should have_link(Page.first.relative_link_to_self)
+  end
+
+  it 'should be able to delete page from list page' do
+    visit '/'
+    fill_in 'name', :with => 'Joana'
+    fill_in 'html', :with => 'oi!'
+    click_button 'Publish my website'
+    visit '/list'
+    @page = Page.first
+    page.should have_content(@page.name)
+    page.should have_link(@page.relative_link_to_self)
+    page.evaluate_script('window.confirm = function() { return true; }')
+    page.find('.delete').click
+    page.should_not have_content(@page.name)
+    page.should_not have_link(@page.relative_link_to_self)
+    visit @page.relative_link_to_self
+    page.should have_content('404 Not found')
   end
 end
