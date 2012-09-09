7201a59615a2e47c3a53ad368576720a96307b13
marano <thiagomarano@gmail.com>
2012-09-09 15:53:00 -0700

    Tooltip for inputs.

--- public/home.js
+++ public/home.js
@@ -4,17 +4,33 @@ $(function () {
   $('#last-name').keypress(validateInput);
   $('#event').keypress(validateInput);
 
+  addTooltip('#name');
+  addTooltip('#middle-initial');
+  addTooltip('#last-name');
+  addTooltip('#event');
+
   $.getJSON('/previous_events', function (events) {
     $('input[name=event]').typeahead({source: events});
   });
 
+  function addTooltip(element) {
+    $(element).tooltip({delay: {show: 800}, placement: 'right', title: 'Only letters and numbers are allowed. Space and underline are not allowed.'});
+  }
+
   function validateInput(event) {
+    if (event.charCode == 13) {
+      return true;
+    }
     var key = event.keyCode || event.which;
     key = String.fromCharCode( key );
-    var regex = /[a-z|A-Z|0-9]|\./;
+    var regex = /[a-z|A-Z|0-9]/;
     if( !regex.test(key) ) {
       event.returnValue = false;
       event.preventDefault();
+      $(event.target).tooltip('show');
+      setTimeout(function () {
+        $(event.target).tooltip('hide');
+      }, 3000);
     }
   }
 });
