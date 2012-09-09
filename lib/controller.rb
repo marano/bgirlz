793f35e9a57caaab3c1d616e01bd4252a00a3c55
marano <thiagomarano@gmail.com>
2012-09-08 17:41:41 -0700

    Will open link if it is a 301 with new location and if it needs https auth.

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'openssl'
+
 class Controller < Sinatra::Base
 
   set :views, 'views'
@@ -29,7 +32,7 @@ class Controller < Sinatra::Base
       return
     end
     if !@link.blank?
-      content = Net::HTTP.get(URI.parse(@link))
+      content = content_from_link(@link)
     elsif !@page.blank?
       content = File.read @page[:tempfile].path
     else
@@ -37,6 +40,27 @@ class Controller < Sinatra::Base
     end
     new_page = Page.create!(:name => @name, :middle_initial => @middle_initial, :last_name => @last_name, :event => @event, :content => content, :enable_comments => @enable_comments == 'on')
     redirect new_page.link_to_self(request) + '?first_time=true'
+  end
+
+  def content_from_link(link)
+    uri = URI.parse(link)
+    http = Net::HTTP.new(uri.host, uri.port) 
+
+    if link =~ /https:/
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+
+    if response.code == '301'
+      new_location = response.header['Location']
+      return content_from_link(new_location)
+    else
+      return response.body
+    end
   end
 
   get '/favicon.ico' do
