23cecb0418bb86e865009c89b689a60cc8f1d514
marano <thiagomarano@gmail.com>
2012-09-11 00:26:03 -0700

    Fixes firefox bug preventing tab to cycle fields.

--- public/home.js
+++ public/home.js
@@ -18,7 +18,7 @@ $(function () {
   }
 
   function validateInput(event) {
-    if (event.charCode == 13) {
+    if (event.charCode == 13 || event.keyCode == 9) {
       return true;
     }
     var key = event.keyCode || event.which;
