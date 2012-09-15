30ba36136920fb8b9889409f389d5df8e3d88127
marano <thiagomarano@gmail.com>
2012-09-14 21:52:47 -0700

    Fix pasted crazy formated code.

--- lib/patch.rb
+++ lib/patch.rb
@@ -1,10 +1,9 @@
 class Array
   def randomize
-    duplicated_original, new_array = self.dup, self.class.new
-    new_array << 
-    duplicated_original.slice!(rand(duplicated_original.size)) until 
-    new_array.size.eql?(self.size)
-    new_array
+    duplicated_original = self.dup
+    new_array = self.class.new
+    new_array << duplicated_original.slice!(rand(duplicated_original.size)) until new_array.size.eql?(self.size)
+    return new_array
   end
 
   def randomize!
