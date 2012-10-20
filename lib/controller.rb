d99f77c2a3b3ab6e46e8fe39ff71de66442024c8
marano <thiagomarano@gmail.com>
2012-10-19 21:24:01 -0700

    What if we allow origin.

--- lib/controller.rb
+++ lib/controller.rb
@@ -76,7 +76,7 @@ class Controller < Sinatra::Base
 
   get '/*/featured' do
     @page = resolve_page_from_path
-    response.headers['X-Frame-Options'] = 'wow!'
+    response.headers['X-Frame-Options'] = 'sameorigin'
     haml :_featured, :layout => false
   end
 
