0ebdd2c19a3f8d9b9f02f16efe1f47236c9a65fc
marano <thiagomarano@gmail.com>
2013-02-19 18:10:35 -0300

    Starts headless if its not a mac.

--- spec_helper.rb
+++ spec_helper.rb
@@ -23,7 +23,7 @@ if ENV['headless'] =~ /false/
   Capybara.current_driver = :selenium
   Capybara.javascript_driver = :selenium
 else
-  Headless.new.start if RbConfig::CONFIG['host_os'] == 'linux'
+  Headless.new.start unless RbConfig::CONFIG['host_os'] === /darwin/
   Capybara.current_driver = :webkit
   Capybara.javascript_driver = :webkit
 end
